aa331ec853f991675d85475e737e01dfe21de8fb
Erich Duda <dudaerich@gmail.com>
2014-06-19 12:24:00 +0200

    Added ResourceFactory server.

---
+++ src/main/java/org/apache/cxf/example/wstransferexample/server/resourcefactory/Server.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.apache.cxf.example.wstransferexample.server.resourcefactory;
+
+import org.apache.cxf.jaxws.JaxWsServerFactoryBean;
+import org.apache.cxf.ws.transfer.manager.MemoryResourceManager;
+import org.apache.cxf.ws.transfer.manager.ResourceManager;
+import org.apache.cxf.ws.transfer.resource.Resource;
+import org.apache.cxf.ws.transfer.resource.ResourceLocal;
+import org.apache.cxf.ws.transfer.resourcefactory.ResourceFactory;
+import org.apache.cxf.ws.transfer.resourcefactory.ResourceFactoryImpl;
+import org.apache.cxf.ws.transfer.resourcefactory.resolver.SimpleResourceResolver;
+
+/**
+ * Server main class.
+ * @author Erich Duda
+ */
+public class Server {
+    
+    private static final String RESOURCE_STUDENTS_URL = "http://localhost:8080/ResourceStudents";
+    
+    private static final String RESOURCE_FACTORY_URL = "http://localhost:8080/ResourceFactory";
+    
+    public static void main(String[] args) {
+        ResourceManager resourceManager = new MemoryResourceManager();
+        createResource(resourceManager);
+        createResourceFactory(resourceManager);
+    }
+    
+    private static void createResource(ResourceManager resourceManager) {
+        ResourceLocal resourceLocal = new ResourceLocal();
+        resourceLocal.setManager(resourceManager);
+        JaxWsServerFactoryBean factory = new JaxWsServerFactoryBean();
+        factory.setServiceClass(Resource.class);
+        factory.setServiceBean(resourceLocal);
+        factory.setAddress(RESOURCE_STUDENTS_URL);
+        factory.create();
+    }
+    
+    private static void createResourceFactory(ResourceManager resourceManager) {
+        ResourceFactoryImpl resourceFactory = new ResourceFactoryImpl();
+        resourceFactory.setResourceResolver(new SimpleResourceResolver(RESOURCE_STUDENTS_URL, resourceManager));
+        JaxWsServerFactoryBean factory = new JaxWsServerFactoryBean();
+        factory.setServiceClass(ResourceFactory.class);
+        factory.setServiceBean(resourceFactory);
+        factory.setAddress(RESOURCE_FACTORY_URL);
+        factory.create();
+    }
+    
+}
